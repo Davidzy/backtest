d8ca847fc4693c43da1904e2855d67c78df96e64
david <freshyu2002@163.com>
2016-12-05 16:19:11 +0800

    portfolio done

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from event import MarketEvent
 
+
 class DataHandler(object):
     """
     DataHandler is an abstract base class providing an interface for
@@ -75,10 +76,13 @@ class HistoricCSVDataHandler(DataHandler):
     """
     def __init__(self, events, csv_dir, symbol_list):
         """
-
-        :param events:
-        :param csv_dir:
-        :param symbol_list:
+        Initialises the historic data handler by requesting
+        the location of the CSV files and a list of symbols.
+        It will be assumed that all files are of the form
+        ’symbol.csv’, where symbol is a string in the list.
+        :param events: The Event Queue.
+        :param csv_dir: Absolute directory path to the CSV files.
+        :param symbol_list: A list of symbol strings.
         """
         self.events = events
         self.csv_dir = csv_dir
@@ -92,14 +96,16 @@ class HistoricCSVDataHandler(DataHandler):
 
     def _open_convert_csv_files(self):
         """
-
-        :return:
+        Opens the CSV files from the data directory, converting
+        them into pandas DataFrames within a symbol dictionary.
+        For this handler it will be assumed that the data is
+        taken from Yahoo. Thus its format will be respected.
         """
         comb_index = None
         for s in self.symbol_list:
             # Load the CSV
             self.symbol_data[s] = pd.read_csv(
-                os.path.join(self.csv_dir, "{fname}.csv".format(fname=symbol)),
+                os.path.join(self.csv_dir, "{fname}.csv".format(fname=s)),
                 parse_dates = True,
                 header=0,
                 index_col=0,
@@ -117,3 +123,89 @@ class HistoricCSVDataHandler(DataHandler):
         for s in self.symbol_list:
             self.symbol_data[s] = self.symbol_data[s].\
                 reindex(index=comb_index, method='pad').iterrows()
+
+    def _get_new_bar(self, symbol):
+        """
+        Returns the latest bar from the data feed.
+        :return:
+        """
+        for b in self.symbol_data[symbol]:
+            yield b
+
+    def get_latest_bar(self, symbol):
+        """
+        Returns the last bar from the latest_symbol list.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print("That symbol is not available in the historical data set.")
+            raise
+        else:
+            return bars_list[-1]
+
+    def get_latest_bars(self, symbol, N=1):
+        """
+        Returns the last N bars from the latest_symbol list,
+        or N-k if less available.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print("That symbol is not available in the historical data set.")
+            raise
+        else:
+            return bars_list[-N:]
+
+    def get_latest_bar_datetime(self, symbol):
+        """
+        Returns a Python datetime object for the last bar.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print("That symbol is not available in the historical data set.")
+            raise
+        else:
+            return bars_list[-1][0]
+
+    def get_latest_bar_value(self, symbol, val_type):
+        """
+        Returns one of the Open, High, Low, Close, Volume or OI
+        values from the pandas Bar series object.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print("That symbol is not available in the historical data set.")
+            raise
+        else:
+            return getattr(bars_list[-1][1], val_type)
+
+    def get_latest_bars_values(self, symbol, val_type, N=1):
+        """
+        Returns the last N bar values from the
+        latest_symbol list, or N-k if less available.
+        """
+        try:
+            bars_list = self.get_latest_bars(symbol, N)
+        except KeyError:
+            print("That symbol is not available in the historical data set.")
+            raise
+        else:
+            return np.array([getattr(b[1], val_type) for b in bars_list])
+
+    def update_bars(self):
+        """
+        Pushes the latest bar to the latest_symbol_data structure
+        for all symbols in the symbol list.
+        """
+        for s in self.symbol_list:
+            try:
+                bar = next(self._get_new_bar(s))
+            except StopIteration:
+                self.continue_backtest = False
+            else:
+                if bar is not None:
+                    self.latest_symbol_data[s].append(bar)
+        self.events.put(MarketEvent())
